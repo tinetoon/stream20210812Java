8c7d6bac8bcf92ac32e4951af84b6d8437b5c9b6
Дюжаков Антон <tinetoon@mail.ru>
2021-09-01 20:33:57 +0300

    А.А. Дюжаков - ДЗ № 6 ч.2

--- src/Lection06/home/HomeWorkApp06.java
+++ src/Lection06/home/HomeWorkApp06.java
@@ -1,5 +1,8 @@
 package Lection06.home;
 
+import Lection06.home.classes.Cats;
+import Lection06.home.classes.Dogs;
+
 /**
  * Ответственный за создание класса - А.А. Дюжаков
  * Дата создания: 31.08.2021
@@ -9,5 +12,32 @@ public class HomeWorkApp06 {
 
     public static void main(String[] args) {
 
+        int swimmingMaxDistanceCat = 0;
+        int swimmingMaxDistanceDog = 10;
+        int runningMaxDistanceCat = 200;
+        int runningMaxDistanceDog = 500;
+
+        int runCat = 155;
+        int runCatBoost = 300;
+        int runDog = 400;
+        int swimDog = 8;
+
+        Cats newCat = new Cats("Васька", swimmingMaxDistanceCat, runningMaxDistanceCat);
+        Cats newHomeCat = new Cats("Мурзик", swimmingMaxDistanceCat, runningMaxDistanceCat);
+        Dogs newDog = new Dogs("Шарик", swimmingMaxDistanceDog, runningMaxDistanceDog);
+
+        System.out.println(newCat.getType() + " по имени " + newCat.getName() +
+                " пробежал " + newCat.run(runCat) + " метров");
+
+        System.out.println(newHomeCat.getType() + " по имени " + newHomeCat.getName() +
+                " пробежал " + newCat.run(runCatBoost) + " метров");
+
+        System.out.println(newDog.getType() + " по имени " + newDog.getName() +
+                " пробежала " + newDog.run(runDog) + " метров");
+        System.out.println(newDog.getType() + " по имени " + newDog.getName() +
+                " проплыла " + newDog.swim(swimDog) + " метров");
+
+        System.out.println("Количество созданных котов: " + Cats.numberCat);
+        System.out.println("Количество созданных собак: " + Dogs.numberDog);
     }
 }
--- src/Lection06/home/classes/Dogs.java
+++ src/Lection06/home/classes/Dogs.java
@@ -2,7 +2,18 @@ package Lection06.home.classes;
 
 public class Dogs extends Animals {
 
-    public Dogs(String name, int swimmingDistance, int runningDistance) {
-        super(name, swimmingDistance, runningDistance);
+    public static int numberDog = 0;
+
+    public Dogs(String name, int swimmingMaxDistance, int runningMaxDistance) {
+        super(name, "Собака", swimmingMaxDistance, runningMaxDistance);
+        numberDog++;
     }
+
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
 }
