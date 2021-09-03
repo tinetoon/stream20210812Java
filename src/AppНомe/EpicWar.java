6f9cda8c55a0514bed10d80a5dbdce5523280ecb
Дюжаков Антон <tinetoon@mail.ru>
2021-09-03 21:20:58 +0300

    А.А. Дюжаков - ДЗ № 7 ч.1

---
+++ src/AppНомe/EpicWar.java
@@ -0,0 +1,14 @@
+package AppНомe;
+
+/**
+ * Ответственный за создание класса - А.А. Дюжаков
+ * Дата создания: 03.09.2021
+ */
+
+public class EpicWar {
+
+    public static void main(String[] args) {
+
+        WindowApp windowApp = new WindowApp();
+    }
+}
