43d61035762d7fdf5b1bd505e2aa15e3901cd7b1
Дюжаков Антон <tinetoon@mail.ru>
2021-09-05 10:21:39 +0300

    А.А. Дюжаков - ДЗ № 7 ч.3

--- src/AppНомe/WindowApp.java
+++ src/AppНомe/WindowApp.java
@@ -26,54 +26,106 @@ public class WindowApp extends JFrame {
     private JButton btnExitGame;
 
     private JPanel gameInfoPanel;
+    private JLabel playerName;
+    private JLabel mapSize;
+    private JLabel countMonster;
 
     private JPanel gamePlayerPanel;
+    private JLabel playerHealthPoint;
+    private JLabel playerAttackPoint;
+
+    private JPanel gameMonsterPanel;
+    private JLabel monsterHealthPoint;
+    private JLabel monsterAttackPoint;
 
     private JPanel gameControllerPanel;
-
     private JPanel gameLogPanel;
 
+    // Конструктор метода окна приложения
     WindowApp() {
 
-        winSetup();
-
+        setupWin(); // Собираем интерфейс из блоков
         map = new GameMap(); // Создаём объект карты в виде панели
-
         setupGui(); // Создаём область графического интерфейса
-
         add(gui, BorderLayout.EAST); // Добавляем графический интерфейс в правую (восточную) часть окна
-        add(map);
-
-        setVisible(true);
+        add(map); // Добавляем панель карты на оставшуюся часть окна
+        setVisible(true); // Делаем окно видимым
     }
 
     // Метод сборки интерфейса
     private void setupGui() {
-        gui = new JPanel();
-        gui.setLayout(new GridLayout(5, 1)); // Изменяем способ размещения панелей
-        setupGameControl(); // Вызываем метод создания блока с кнопками Старт/Выход
-        gui.add(gameControlPanel); // Добавляем на панель интерфейса панель с кнопками
+        gui = new JPanel(); // Создаём новую панель для графического интерфейса
+        gui.setLayout(new GridLayout(6, 1)); // Изменяем способ размещения панелей
+
+        setupGameControl(); // 1. Вызываем метод создания блока с кнопками Старт/Выход
+        setupGameInfo(); // 2. Вызываем метод создания блока с информацией об игре
+        setupPlayerInfo(); // 3. Вызываем метод создания блока с информацией об игроке
+        setupMonsterInfo(); // 4. Вызываем метод создания блока с информацией о врагах
+
+        gui.add(gameControlPanel); // 1. Добавляем на панель интерфейса панель с кнопками
+        gui.add(gameInfoPanel); // 2. Добавляем на панель интерфейса панель с информацией об игре
+        gui.add(gamePlayerPanel); // 3. Добавляем на панель интерфейса панель с информацией об игроке
+        gui.add(gameMonsterPanel); // 4. Добавляем на панель интерфейса панель с информацией о врагах
     }
 
-    // Создаём блок запуска и выхода из игры
+    // 1. Создаём блок запуска и выхода из игры
     private void setupGameControl() {
         gameControlPanel = new JPanel();
-        gameControlPanel.setLayout(new GridLayout(3, 1));
+        gameControlPanel.setLayout(new GridLayout(1, 1)); // Располагаем кнопки в одну строку
 
         btnStartGame = new JButton("Запуск игры");
         btnExitGame = new JButton("Выход из игры");
 
-        gameControlPanel.add(new JLabel("=== Меню Игры ==="));
         gameControlPanel.add(btnStartGame);
         gameControlPanel.add(btnExitGame);
     }
 
+    // 2. Создаём блок информации об игре:
+    //      - имя игрока;
+    //      - размер карты;
+    //      - количество врагов.
     private void setupGameInfo() {
+        gameInfoPanel = new JPanel();
+        gameInfoPanel.setLayout(new GridLayout(4, 1));
 
+        playerName = new JLabel("Игрок: ");
+        mapSize = new JLabel("Создана карта размером: ");
+        countMonster = new JLabel("Создано врагов: ");
+
+        gameInfoPanel.add(new JLabel("=== Информация ==="));
+        gameInfoPanel.add(playerName);
+        gameInfoPanel.add(mapSize);
+        gameInfoPanel.add(countMonster);
     }
 
+    // 3. Создаём блок информации об игроке:
+    //      - уровень здоровья;
+    //      - сила;
     private void setupPlayerInfo() {
+        gamePlayerPanel = new JPanel();
+        gamePlayerPanel.setLayout(new GridLayout(3, 1));
 
+        playerAttackPoint = new JLabel("Сила игрока: ");
+        playerHealthPoint = new JLabel("Очки здоровья игрока: ");
+
+        gamePlayerPanel.add(new JLabel("=== Информация об игроке ==="));
+        gamePlayerPanel.add(playerAttackPoint);
+        gamePlayerPanel.add(playerHealthPoint);
+    }
+
+    // 3. Создаём блок информации о врагах:
+    //      - уровень здоровья;
+    //      - сила;
+    private void setupMonsterInfo() {
+        gameMonsterPanel = new JPanel();
+        gameMonsterPanel.setLayout(new GridLayout(3, 1));
+
+        monsterAttackPoint = new JLabel("Сила врагов: ");
+        monsterHealthPoint = new JLabel("Очки здоровья врагов: ");
+
+        gameMonsterPanel.add(new JLabel("=== Информация о врагах ==="));
+        gameMonsterPanel.add(monsterAttackPoint);
+        gameMonsterPanel.add(monsterHealthPoint);
     }
 
     private void setupPlayerController() {
@@ -85,7 +137,7 @@ public class WindowApp extends JFrame {
     }
 
     // Настройки окна
-    private void winSetup() {
+    private void setupWin() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(winPosX, winPosY);
         setSize(winWidth, winHeight);
