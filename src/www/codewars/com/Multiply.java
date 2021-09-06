6c79fd30b45ae80e6d0978193ce97f61a8a0e89a
Дюжаков Антон <tinetoon@mail.ru>
2021-09-06 19:42:31 +0300

    Доп.обучение - ч.1

---
+++ src/www/codewars/com/Multiply.java
@@ -0,0 +1,18 @@
+package www.codewars.com;
+
+public class Multiply {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 5;
+        System.out.println("Произведение двух чисел " + a + " * " + b + " = " + multiply(a, b));
+
+        String name = "Антон";
+        int age = 39;
+        System.out.printf("Имя: %s \tВозраст: %d\n", name, age);
+        // Выводит в консоль следующую строку ==> Имя: Антон 	Возраст: 39
+    }
+
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
---
+++ src/www/codewars/com/Study.java
@@ -0,0 +1,4 @@
+package www.codewars.com;
+
+public class Study {
+}
